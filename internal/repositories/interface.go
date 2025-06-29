5db3a9378daf015f5e37af2751efa3ff8aed5182
fabiokusaba <fabinhokb44@gmail.com>
2025-06-29 10:47:16 -0300

    refactor: change param to entities.Category

--- internal/repositories/interface.go
+++ internal/repositories/interface.go
@@ -1,5 +1,7 @@
 package repositories
 
+import "github.com/fabiokusaba/go-categories-msvc/internal/entities"
+
 type ICategoryRepository interface {
-	Save(name string) error
+	Save(category *entities.Category) error
 }
