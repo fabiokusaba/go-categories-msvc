636d14403299e5569da4ddf9ddf86f596dfb23c4
fabiokusaba <fabinhokb44@gmail.com>
2025-06-28 23:00:54 -0300

    feat: add create category use case

---
+++ internal/use-cases/create-category.go
@@ -0,0 +1,26 @@
+package usecases
+
+import (
+	"log"
+
+	"github.com/fabiokusaba/go-categories-msvc/internal/entities"
+)
+
+type createCategoryUseCase struct {
+	// db
+}
+
+func NewCreateCategoryUseCase() *createCategoryUseCase {
+	return &createCategoryUseCase{}
+}
+
+func (u *createCategoryUseCase) Execute(name string) error {
+	category, err := entities.NewCategory(name)
+	if err != nil {
+		return err
+	}
+
+	// TODO: persist entity to db
+	log.Println(category)
+	return nil
+}
